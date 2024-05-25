ddf051730bbb9c9e817e9243410143bdf9df1eb2
eric <ericibrahim19@gmail.com>
2024-05-24 23:35:13 -0300

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
+import Navbar from './components/Navbar.jsx'
 import './css/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>,
 )
