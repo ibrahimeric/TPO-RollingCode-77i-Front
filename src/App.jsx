15a1c21fd74e6ec280dda2834387e01d13a47340
eric <ericibrahim19@gmail.com>
2024-05-26 16:30:41 -0300

    orden carpetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from '/react.svg'
 import viteLogo from '/vite.svg'
-import '../css/App.css'
+import './css/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
