ab8ea7578aebab08a8dfa210f3f52b59b55701f3
eric <ericibrahim19@gmail.com>
2024-05-26 16:29:03 -0300

    orden carpetas

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
+import App from './App'
 import './css/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
