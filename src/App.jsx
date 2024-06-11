c8373953becfee221ccc0bf399c5217cc11acfcf
eric <ericibrahim19@gmail.com>
2024-06-11 01:31:57 -0300

    page turnos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-// import Contact from './components/Contact';
-// import Home from './components/Home';
-// import Navbar from './components/Navbar';
-// import Formulario from './components/Form';
 import FormTurnos from './components/FormTurnos';
 
 
@@ -14,8 +10,6 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        {/* <Route path="/form" element={<Form contacto={form} />} /> */}
         <Route path="/formTurnos" element={<FormTurnos setFormTurnos={formTurnos} />} />
       </Routes>
     </div>
--- src/components/FormTurnos.jsx
+++ src/components/FormTurnos.jsx
@@ -1,17 +1,13 @@
 import React, { useState } from 'react';
-import { Modal, Form, Button } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import '../css/FormTurnos.css'
 
 function FormTurnos() {
-    const [show, setShow] = useState(false);
     const [formData, setFormData] = useState({
         name: '',
         email: '',
         message: ''
     });
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -22,94 +18,88 @@ function FormTurnos() {
         e.preventDefault();
         // Aquí puedes agregar la lógica para enviar el formulario
         console.log(formData);
-        handleClose(); // Cerrar el modal después de enviar el formulario
+        // Redireccionar a una nueva página después de enviar el formulario
+        window.location.href = '/nueva-pagina'; // Reemplaza '/nueva-pagina' con la URL de tu nueva página
     };
 
-
     return (
-        <>
-            <Button variant="primary" onClick={handleShow}>
-                Ahora Solicita tu Turno
-            </Button>
-
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header className='modal-header-shifts' closeButton>
-                    <div className='title-form-shifts'>
-                        <Modal.Title>Solicitud de Turno</Modal.Title>
+        <div className="container">
+            <div className="left-column">
+                <h1>Solicite un Turno</h1>
+                <p>Comuníquese con nosotros para todas sus necesidades de registro, adopción y vacunación de mascotas en Concepción.</p>
+            </div>
+            <div className="right-column">
+                <div className="card">
+                    <div className="card-header">
+                        <h1>Solicitud de Turno</h1>
                     </div>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group className="mb-3" controlId="formName">
-                            <Form.Label>Nombre</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Ingresa tu nombre"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
+                    <div className="card-body">
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group className="mb-3" controlId="formName">
+                                <Form.Label>Nombre</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Ingresa tu nombre"
+                                    name="name"
+                                    value={formData.name}
+                                    onChange={handleChange}
+                                />
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formEmail">
-                            <Form.Label>Correo</Form.Label>
-                            <Form.Control
-                                type="email"
-                                placeholder="Ingresa tu email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                            />
-                            <Form.Text className="text-muted">
-                                Nunca compartiremos tu email con nadie.
-                            </Form.Text>
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="formEmail">
+                                <Form.Label>Correo</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Ingresa tu email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                />
+                                <Form.Text className="text-muted">
+                                    Nunca compartiremos tu email con nadie.
+                                </Form.Text>
+                            </Form.Group>
 
+                            <Form.Group className="mb-3" controlId="formOptionsMascotas">
+                                <Form.Label>Mascotas</Form.Label>
+                                <Form.Select onChange={handleChange}>
+                                    <option value="option1">Opción 1</option>
+                                    <option value="option2">Opción 2</option>
+                                </Form.Select>
+                            </Form.Group>
 
+                            <Form.Group className="mb-3" controlId="formOptionsTurnos">
+                                <Form.Label>Turnos</Form.Label>
+                                <Form.Select onChange={handleChange}>
+                                    <option value="option1">Castración</option>
+                                    <option value="option2">Vacunación</option>
+                                </Form.Select>
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formOptions">
-                            <Form.Label>Mascotas</Form.Label>
-                            <Form.Select onChange={handleChange}>
-                                <option value="option1">Opción 1</option>
-                                <option value="option2">Opción 2</option>
-                            </Form.Select>
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="formDate">
+                                <Form.Label>Selecciona una fecha</Form.Label>
+                                <Form.Control type="date" />
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formOptions">
-                            <Form.Label>Turnos</Form.Label>
-                            <Form.Select onChange={handleChange}>
-                                <option value="option1">Castración</option>
-                                <option value="option2">Vacunación</option>
-                            </Form.Select>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formDate">
-                            <Form.Label>Selecciona una fecha</Form.Label>
-                            <Form.Control type="date" />
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formMessage">
-                            <Form.Label>Mensaje</Form.Label>
-                            <Form.Control
-                                as="textarea"
-                                rows={3}
-                                placeholder="Ingresa un Mensaje"
-                                name="message"
-                                value={formData.message}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
-
-                        <Button variant="secondary" onClick={handleClose}>
-                            Cancelar
-                        </Button>
-                        <Button variant="primary" type="submit">
-                            Enviar
-                        </Button>
-                    </Form>
-                </Modal.Body>
-            </Modal>
-        </>
+                            <Form.Group className="mb-3" controlId="formMessage">
+                                <Form.Label>Mensaje</Form.Label>
+                                <Form.Control
+                                    as="textarea"
+                                    rows={3}
+                                    placeholder="Ingresa un Mensaje"
+                                    name="message"
+                                    value={formData.message}
+                                    onChange={handleChange}
+                                />
+                            </Form.Group>
+                            <Button variant="primary" type="submit" className='btn-enviar'>
+                                Enviar
+                            </Button>
+                        </Form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
