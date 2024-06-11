5a1b9115240c5c181f6a902f2adf3d6d75f7bc3f
eric <ericibrahim19@gmail.com>
2024-06-11 15:14:03 -0300

    redireccionamiento de rutas y formularios

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom';
-import NavBar from './components/Navbar';
 import Slider from './components/Slider';
 import Cards from './components/Cards'; // Importamos el componente Cards
 import UserExperience from './components/UserExperience';
-import Footer from './components/Footer.jsx';
-import { menuItems } from './js/data';
 import './css/App.css'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado de autenticación del usuario
-  const [isAdmin, setIsAdmin] = useState(false);
-
-  // Función para cerrar sesión
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    setIsAdmin(false); // Asegúrate de actualizar el estado de administrador al cerrar sesión
-  };
-
   return (
     <>
-      <Routes>
-        <Route path="/" element={<NavBar menuItems={menuItems} isLoggedIn={isLoggedIn} isAdmin={isAdmin} />} />
-        <Route path="/" element={<Link to="/non-existent-page">Go to 404</Link>} />
-      </Routes>
       <Slider />
       <Cards />
       <UserExperience />
-      <Footer />
     </>
   );
 }
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,17 +1,20 @@
+// Router.jsx
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
+import NavBar from './components/Navbar'; // Importa el NavBar
+import Footer from './components/Footer'; // Importa el Footer
 import Error404 from './components/Error404';
 import Loading from './components/Loading';
 import Contact from './components/Contact';
 import PetsPage from './components/PetsPage';
 import TurnosPage from './components/TurnosPage';
 import FormTurnos from './components/FormTurnos';
-
+import FormRegistro from './components/FormRegistro';
+import FormAdopcion from './components/FormAdopcion';
 
 const AppRouter = () => {
   const [isLoading, setIsLoading] = useState(true);
-    const [formTurnos, setFormTurnos] = useState(false);
 
   useEffect(() => {
     // Simula el tiempo de carga de la página
@@ -23,21 +26,24 @@ const AppRouter = () => {
     return () => clearTimeout(timeout);
   }, []);
 
-
   return (
     <React.StrictMode>
       {isLoading ? ( // Si isLoading es verdadero, muestra el componente de carga
         <Loading />
       ) : (
         <Router>
+          <NavBar /> {/* Renderiza el NavBar en todas las rutas */}
           <Routes>
             <Route path="/" element={<App />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<Error404 />} />
             <Route path="/page-mascotas" element={<PetsPage />} />
             <Route path="/page-turnos" element={<TurnosPage />} />
-            <Route path="/formTurnos" element={<FormTurnos setFormTurnos={formTurnos} />} />
+            <Route path="/formTurnos" element={<FormTurnos />} />
+            <Route path="/formRegistro" element={<FormRegistro />} />
+            <Route path="/formAdopcion" element={<FormAdopcion />} />
           </Routes>
+          <Footer /> {/* Renderiza el Footer en todas las rutas */}
         </Router>
       )}
     </React.StrictMode>
