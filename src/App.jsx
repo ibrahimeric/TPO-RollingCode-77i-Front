93299f736396e93414be2e896e398165f6c0b730
eric <ericibrahim19@gmail.com>
2024-06-12 11:05:20 -0300

    actualizacion admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { Link } from 'react-router-dom';
+
 
 function App() {
 
   return (
     <>
-      <h1>Home Page</h1>
-      {/* Rutas para los componentes */}
-      <Link to="/">Go to Link</Link>
     </>
   )
 }
